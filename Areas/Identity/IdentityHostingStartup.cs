0b93e077895204f4fcdfbc9f952f3e4b3de33852
foeu <fabiano.junior@gft.com>
2020-10-23 16:42:43 -0300

    Subindo projeto para GitLab

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using treino_mvc.Data;
+
+[assembly: HostingStartup(typeof(treino_mvc.Areas.Identity.IdentityHostingStartup))]
+namespace treino_mvc.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
